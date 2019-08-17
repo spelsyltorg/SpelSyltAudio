f83b2f00f55c9dc8b850c417e614a3b194767733
Kasper Esbjörnsson <kasper.esbjornsson@gmail.com>
2019-08-17 23:51:59 +0100

    New wav file, looping test in test project

--- SpelSyltAudio/SpelSyltAudioTestProgram/Source.cpp
+++ SpelSyltAudio/SpelSyltAudioTestProgram/Source.cpp
@@ -2,6 +2,8 @@
 #include "../SpelSyltAudio/Engine/Public/AudioEngine.h"
 #include "../SpelSyltAudio/Engine/Public/SoundSource.h"
 
+#include <chrono>
+
 #pragma comment(lib, "SpelSyltAudio_Debug")
 
 int main()
@@ -11,13 +13,27 @@ int main()
 	SSAL::CSoundSource Source = AE.MakeSource();
 
 	SSAL::SWavFile WavFile;
-	WavFile.LoadFromFile("Norrsken.wav");
+	WavFile.LoadFromFile("eagle.wav");
 
-	Source.PlaySound(WavFile);
+	AE.SetListenerPosition(0.f, 0.f, 0.f);
+	Source.SetPosition(0.f, 0.f, 0.f);
+
+	Source.PlaySound(WavFile, true);
+
+	auto CurrentTime = std::chrono::high_resolution_clock::now();
+	auto PreviousTime = CurrentTime;
+	float DT = 0.f;
+	float TotalTime = 0.f;
 
 	while (true)
 	{
+		CurrentTime = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double> DT = std::chrono::duration_cast<std::chrono::duration<double>>(CurrentTime - PreviousTime);
+		TotalTime += (float)DT.count();
+		
+		Source.SetPosition(0.f, 0.f, sinf(TotalTime) * 10.f);
 
+		PreviousTime = CurrentTime;
 	}
 
 	return 0;
