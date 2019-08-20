8e4b9073f96e52ee9d201c0710f37dfbdb637114
Kasper Esbjörnsson <kasper.esbjornsson@gmail.com>
2019-08-20 22:25:56 +0100

    Music test in testproj, ogg test file and vcxprojs

--- SpelSyltAudio/SpelSyltAudioTestProgram/Source.cpp
+++ SpelSyltAudio/SpelSyltAudioTestProgram/Source.cpp
@@ -1,6 +1,7 @@
 #include "../SpelSyltAudio/FileFormats/Public/WavFile.h"
 #include "../SpelSyltAudio/Engine/Public/AudioEngine.h"
 #include "../SpelSyltAudio/Engine/Public/SoundSource.h"
+#include "../SpelSyltAudio/Engine/Public/MusicSource.h"
 
 #include <chrono>
 
@@ -9,41 +10,47 @@
 int main()
 {
 	SSAL::CAudioEngine AE;
-	AE.Initialize(1);
-	SSAL::CSoundSource Source = AE.MakeSource();
+	AE.Initialize(200);
+	SSAL::CMusicSource Source = AE.MakeMusicSource();
+	Source.OpenOGGStream("Norrsken.ogg");
 
-	SSAL::SWavFile WavFile;
-	WavFile.LoadFromFile("Norrsken.wav");
+	//SSAL::SWavFile WavFile;
+	//WavFile.LoadFromFile("Norrsken.wav");
 
 	AE.SetListenerPosition(0.f, 0.f, 0.f);
-	Source.SetPosition(0.f, 0.f, 0.f);
+	//Source.SetPosition(0.f, 0.f, 0.f);
+	//
+	//Source.SetVolume(1.f);
+	//Source.PlaySound(WavFile, true);
 
-	Source.SetVolume(1.f);
-	Source.PlaySound(WavFile, true);
+	Source.Play();
+	Source.SetVolume(0.25f);
 
-	auto CurrentTime = std::chrono::high_resolution_clock::now();
-	auto PreviousTime = CurrentTime;
-	float DT = 0.f;
-	float TotalTime = 0.f;
+	//auto CurrentTime = std::chrono::high_resolution_clock::now();
+	//auto PreviousTime = CurrentTime;
+	//float DT = 0.f;
+	//float TotalTime = 0.f;
 
 	while (true)
 	{
-		CurrentTime = std::chrono::high_resolution_clock::now();
-		std::chrono::duration<double> DT = std::chrono::duration_cast<std::chrono::duration<double>>(CurrentTime - PreviousTime);
-		TotalTime += (float)DT.count();
+		//CurrentTime = std::chrono::high_resolution_clock::now();
+		//std::chrono::duration<double> DT = std::chrono::duration_cast<std::chrono::duration<double>>(CurrentTime - PreviousTime);
+		//TotalTime += (float)DT.count();
 
-		Source.SetPosition(0.f, 0.f, sinf(TotalTime) * 10.f);
+		Source.Tick();
 
-		PreviousTime = CurrentTime;
-
-		if (TotalTime >= 5.f && TotalTime <= 10.f)
-		{
-			Source.Stop();
-		}
-		else if (TotalTime >= 10.f)
-		{
-			Source.Play();
-		}
+		//Source.SetPosition(0.f, 0.f, sinf(TotalTime) * 10.f);
+		//
+		//PreviousTime = CurrentTime;
+		//
+		//if (TotalTime >= 5.f && TotalTime <= 10.f)
+		//{
+		//	Source.Stop();
+		//}
+		//else if (TotalTime >= 10.f)
+		//{
+		//	Source.Play();
+		//}
 	}
 
 	return 0;
